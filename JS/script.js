6ce81cf67e27c555a4ec08f6d4c08dc531921bdd
sarakarlsen <71293547+sarakarlsen@users.noreply.github.com>
2021-04-29 09:45:12 +0200

    cors

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 const postsContainer = document.querySelector(".container");
 const postsContainerCat = document.querySelector(".test-container");
 
+const corsEnabledUrl = "https://noroffcors.herokuapp.com/" + url;
+
 const url = "https://tsh.olx.mybluehost.me/wp-json/wp/v2/posts?";
 
 const postsIndex = "per_page=5";
